e7e001095cf032d02af6ebdd56407e149d47fb30
Vikas K U <vikasku@thoughtworks.com>
2016-01-21 11:28:24 +0530

    added localhost ip in the server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 var http = require('http');
 var controller = require('./routes');
-http.createServer(controller).listen(process.env.OPENSHIFT_NODEJS_PORT||process.env.PORT||8080,process.env.OPENSHIFT_NODEJS_IP);
+http.createServer(controller).listen(process.env.OPENSHIFT_NODEJS_PORT||process.env.PORT||8080,process.env.OPENSHIFT_NODEJS_IP||'127.0.0.1');
 console.log('server Started at ',process.env.OPENSHIFT_NODEJS_PORT||process.env.PORT||8080);
