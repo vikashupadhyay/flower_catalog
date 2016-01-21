d48825cb8c2e5a340ccb92536a4d97a2df3ff4f4
vikashupadhyay <vikasku@thoughtworks.com>
2016-01-21 11:05:46 +0530

    Updated server.js Port no.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
 var http = require('http');
 var controller = require('./routes');
-http.createServer(controller).listen(process.env.OPENSHIFT_NODEJS_PORT||process.env.PORT||5000,process.env.OPENSHIFT_NODEJS_IP);
+http.createServer(controller).listen(process.env.OPENSHIFT_NODEJS_PORT||process.env.PORT||8080,process.env.OPENSHIFT_NODEJS_IP);
+console.log('server Started at ',process.env.OPENSHIFT_NODEJS_PORT||process.env.PORT||8080);
